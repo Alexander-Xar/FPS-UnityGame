403682e46f0e7e991a74786b4b702fdc4cc2f5f0
ALEX\alepc <alepcharitidis@gmail.com>
2024-04-05 19:06:14 +0300

    added some basic movement animations to the player character

---
+++ FpsUnityGame/Assets/Scripts/MovementStates/MovementBaseState.cs
@@ -0,0 +1,11 @@
+
+public abstract class MovementBaseState 
+{
+
+    public abstract void EnterState(MovementStateManager movement);
+
+
+    public abstract void UpdateState(MovementStateManager movement);
+
+
+}
