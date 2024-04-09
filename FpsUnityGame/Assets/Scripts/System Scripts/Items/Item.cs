cfad9f8ba3677d90dc770ccd8bd2feff7ca3b670
Nikolaos Charitidis <nikoscharits@yahoo.com>
2024-04-09 17:33:01 +0300

    Equipment system, equipment interactions

---
+++ FpsUnityGame/Assets/Scripts/System Scripts/Items/Item.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+/* The base item class. All items should deriv from this */
+[CreateAssetMenu(fileName = "New Item", menuName = "Intentory/Item")]
+public class Item : ScriptableObject
+{
+    new public string name = "New Item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+
+    //called when the item is pressed in the inventory
+    public virtual void Use()
+    {
+
+
+        Debug.Log("Using " + name);
+    }
+
+    public void RemoveFromInventory()
+    {
+        Inventory.instance.Remove(this);
+    }
+}
